2bd2fbb328d481249a2b638024491bbc600f1018
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2025-08-26 06:49:25 +0300

    feat: add function tools to get access to all the course information and include knowledge base in instructions

---
+++ waga_agent/agent.py
@@ -0,0 +1,58 @@
+import json
+from google.adk.agents import Agent
+
+
+def get_user_info():
+    """
+    Retrieves the name of the user that's signed into the site.
+
+    Args:
+        NONE
+    """
+    return {"name": "Dagmawi Solomon"}
+
+
+def get_courses():
+    """
+    Retrieves information about all the published courses 
+
+
+    """
+    with open("courses_rows.json", "r") as f:
+        courses = json.load(f)
+
+    return {"courses": courses}
+
+
+with open("knowledgeBase.txt", "r") as c:
+    knowledge_base = c.read()
+
+
+root_agent = Agent(
+    name="waga_agent",
+    model="gemini-2.0-flash",
+    description=(
+        "Agent to provide guidance to users, answer questions based on our data, give them technical support and connect their message to a human."
+    ),
+    instruction=(
+        f"""You are a helpful agent for WAGA Academy. Your primary goal is to provide excellent customer service by answering questions, offering guidance, and providing technical support.
+
+Your first action in any new conversation must be to greet the user warmly, introduce yourself as the WAGA Academy assistant, and briefly describe how you can help. After that, you should perform a function call to 'get_user_info' to personalize the interaction.
+
+Example of a first message:
+'Hello, John Doe! ☕ I'm your WAGA Academy assistant, here to bridge the gap between traditional coffee farming and Web3 innovation. How can I help you today?'
+
+Do not wait for user input before sending this greeting and calling the tool. You should be the first one to send the message
+
+RULES FOR RESPONSES.
+1) Be as brief and concise as possible
+2) Don't Hallucinate if you don't know the answer clearly state it and suggest to connect to a human
+3) If the user asks about topics that's not related to waga academy respectfully let them know it's out of your scope
+
+Here is the knowledge base for waga_academy {knowledge_base}
+
+If you are asked questions about the courses you  can call the 'get_courses' function to get all the available courses
+"""
+    ),
+    tools=[get_user_info, get_courses]
+)
